ce380713804f5b2ea79e963779507da965df7b1f
Mike D <akagetsu@gmail.com>
2015-11-03 21:54:13 +0000

    Implement prototype mouse movement stuffs

--- mousemove.js
+++ mousemove.js
@@ -17,14 +17,27 @@ pc.script.create("mousemove", function (app) {
 			}, this);
 		},
 		update: function(dt) {
-			this.camera.setLocalEulerAngles(this.eulers.y, this.eulers.x, 0);
+			if(!this.camera) {
+				this._createCamera();
+			}
+
+			this.entity.setLocalEulerAngles(0, this.eulers.x, 0);
+			this.camera.setLocalEulerAngles(this.eulers.y, 0, 0);
 		},
 		_onMouseMove: function(e) {
 			if(pc.Mouse.isPointerLocked() || e.buttons[0]) {
 				this.eulers.x -= this.lookSpeed * e.dx;
-				this.eulers.y -= this.lookspeed * e.dy;
+				this.eulers.y -= this.lookSpeed * e.dy;
 			}
-		}
+		},
+		_createCamera: function () {
+            // If user hasn't assigned a camera, create a new one
+            this.camera = new pc.Entity();
+            this.camera.setName("First Person Camera");
+            this.camera.addComponent("camera");
+            this.entity.addChild(this.camera);
+            this.camera.translateLocal(0, 0.5, 0);
+        }
 	};
 	return Mousemove;
 });
