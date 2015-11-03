56ca9f590b01109faa7aa043360202f3f1707198
Mike D <akagetsu@gmail.com>
2015-11-03 21:23:02 +0000

    Prototype mousemove

---
+++ mousemove.js
@@ -0,0 +1,30 @@
+pc.script.attribute("camera", "entity", null);
+pc.script.attribute("lookSpeed", "number", 0.5);
+
+pc.script.create("mousemove", function (app) {
+	var Mousemove = function(entity) {
+		this.entity = entity;
+
+		this.camera = null;
+		this.eulers = new pc.Vec3();
+	};
+	Mousemove.prototype = {
+		initialize: function() {
+			app.mouse.on("mousemove", this._onMouseMove, this);
+
+			app.mouse.on("mousedown", function() {
+				app.mouse.enablePointerLock();
+			}, this);
+		},
+		update: function(dt) {
+			this.camera.setLocalEulerAngles(this.eulers.y, this.eulers.x, 0);
+		},
+		_onMouseMove: function(e) {
+			if(pc.Mouse.isPointerLocked() || e.buttons[0]) {
+				this.eulers.x -= this.lookSpeed * e.dx;
+				this.eulers.y -= this.lookspeed * e.dy;
+			}
+		}
+	};
+	return Mousemove;
+});
