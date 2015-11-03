d8038a1c45d50f3e375992578c17c5763a241b5e
Mike D <akagetsu@gmail.com>
2015-11-03 22:04:59 +0000

    Remove some unnecessary stuff from the mouse movement

--- mousemove.js
+++ mousemove.js
@@ -17,10 +17,6 @@ pc.script.create("mousemove", function (app) {
 			}, this);
 		},
 		update: function(dt) {
-			if(!this.camera) {
-				this._createCamera();
-			}
-
 			this.entity.setLocalEulerAngles(0, this.eulers.x, 0);
 			this.camera.setLocalEulerAngles(this.eulers.y, 0, 0);
 		},
@@ -29,15 +25,7 @@ pc.script.create("mousemove", function (app) {
 				this.eulers.x -= this.lookSpeed * e.dx;
 				this.eulers.y -= this.lookSpeed * e.dy;
 			}
-		},
-		_createCamera: function () {
-            // If user hasn't assigned a camera, create a new one
-            this.camera = new pc.Entity();
-            this.camera.setName("First Person Camera");
-            this.camera.addComponent("camera");
-            this.entity.addChild(this.camera);
-            this.camera.translateLocal(0, 0.5, 0);
-        }
+		}
 	};
 	return Mousemove;
 });
