fc5dfbe48b2d5a15adc254ad696aefa498d39f04
Mike D <akagetsu@gmail.com>
2015-11-11 20:28:20 +0000

    Start adding control functionality in the player class

--- player.js
+++ player.js
@@ -4,10 +4,15 @@ pc.script.attribute('speed', 'number', 1, {
     min: 0.1,
     max: 1
 });
+pc.script.attribute("lookSpeed", "number", 0.5);
 
 pc.script.create("player", function(app) {
     var Player = function(entity) {
         this.entity = entity;
+
+        this.camera = null;
+
+        this.eulers = new pc.Vec3();
 
         // Disable browser default behaviour when we right click
         app.mouse.disableContextMenu();
@@ -17,16 +22,16 @@ pc.script.create("player", function(app) {
 
         // Listen for mouseclicks and handle them accordingly
         app.mouse.on(pc.EVENT_MOUSEDOWN, this.onMouseDown, this);
-
-        // Listen for keypress events and handle them accordingly
-        app.keyboard.on(pc.EVENT_KEYDOWN, this.onKeyDown, this);
-
     };
 
     Player.prototype = {
         name: "PlayerName",
         initialize: function() {
-            this.speed = 0.5;
+            this.camera = this.entity.findByName("Camera");
+        },
+        update: function(dt) {
+            this.handleMovement();
+            this.handleLooking();
         },
         strafe: function(direction) {
             if (direction === "left") {
@@ -42,14 +47,33 @@ pc.script.create("player", function(app) {
                 this.entity.translateLocal(0, 0, this.speed);
             }
         },
+        handleLooking: function() {
+            this.entity.setLocalEulerAngles(0, this.eulers.x, 0);
+            this.camera.setLocalEulerAngles(this.eulers.y, 0, 0);
+            this.camera.setPosition(this.entity.getPosition());
+            this.camera.translateLocal(0, 2, 10);
+        },
         onMouseMove: function(event) {
-
+            if (pc.Mouse.isPointerLocked() || event.buttons[0]) {
+                this.eulers.x -= this.lookSpeed * event.dx;
+                this.eulers.y -= this.lookSpeed * event.dy;
+            }
         },
         onMouseDown: function(event) {
-
+            app.mouse.enablePointerLock();
         },
-        onKeyDown: function(event) {
+        handleMovement: function() {
+            if (app.keyboard.isPressed(pc.KEY_A)) {
+                this.strafe("left");
+            } else if (app.keyboard.isPressed(pc.KEY_D)) {
+                this.strafe("right");
+            }
 
+            if (app.keyboard.isPressed(pc.KEY_W)) {
+                this.walk("forward");
+            } else if (app.keyboard.isPressed(pc.KEY_S)) {
+                this.walk("backward");
+            }
         }
     };
 
