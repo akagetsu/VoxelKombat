2ce5f0c183636b8cd95fb0ad8c03e34a5a2adb18
Mike D <akagetsu@gmail.com>
2015-11-24 20:23:44 +0000

    Split player and camera

---
+++ camera.js
@@ -0,0 +1,60 @@
+pc.script.attribute("lookSpeed", "number", 0.9);
+
+pc.script.attribute("player", "entity", null);
+
+pc.script.create("camera", function(app) {
+	var Camera = function(entity) {
+		this.player = entity; //this is the camera
+
+		this.player = null;
+
+		this.eulers = new pc.vec3();
+
+		// Disable browser default behaviour when we right click
+        app.mouse.disableContextMenu();
+
+        // Listen for mousemovement and handle it accordingly
+        app.mouse.on(pc.EVENT_MOUSEMOVE, this.onMouseMove, this);
+
+        // Listen for mouseclicks and handle them accordingly
+        app.mouse.on(pc.EVENT_MOUSEDOWN, this.onMouseDown, this);
+	};
+
+	Camera.prototype = {
+		name: "PlayerCamera",
+		initialize: function() {},
+		update: function(dt) {
+			this.handleCamera();
+		},
+		handleCamera: function() {
+            var cameraPosition = this.entity.getPosition();
+            this.entity.setLocalEulerAngles(this.eulers.y, this.eulers.x, 0);
+            this.entity.setPosition(this.player.getPosition());
+            this.entity.translateLocal(0, 2, 10);
+            this.entity.rotateLocal(-15, 0, 0);
+            if (this.entity.getPosition().y <= 0) {
+                this.entity.setPosition(cameraPosition.x, 0, cameraPosition.z);
+            }
+        },
+        onMouseMove: function(event) {
+            if (pc.Mouse.isPointerLocked() || event.buttons[0]) {
+                this.eulers.x -= this.lookSpeed * event.dx;
+                this.eulers.y -= this.lookSpeed * event.dy;
+
+                // clip camera on top
+                if (this.eulers.y <= -75) {
+                    this.eulers.y = -75;
+                } else if (this.eulers.y >= 45) {
+                    this.eulers.y = 45;
+                }
+                // reset left-right to 0 once a full circle is complete
+                if (this.eulers.x >= 360 || this.eulers.x <= -360) {
+                    this.eulers.x = 0;
+                }
+            }
+        },
+        onMouseDown: function(event) {
+            app.mouse.enablePointerLock();
+        },
+	};
+});
--- player.js
+++ player.js
@@ -1,70 +1,18 @@
 // player.js
-// move factor attribute
-pc.script.attribute("lookSpeed", "number", 0.9);
-
 pc.script.attribute("power", "number", 10000);
-
-pc.script.attribute("camera", "entity", null);
 
 pc.script.create("player", function(app) {
     var force = new pc.Vec3();
 
     var Player = function(entity) {
         this.entity = entity;
-
-        this.camera = null;
-
-        this.eulers = new pc.Vec3();
-
-
-        // Disable browser default behaviour when we right click
-        app.mouse.disableContextMenu();
-
-        // Listen for mousemovement and handle it accordingly
-        app.mouse.on(pc.EVENT_MOUSEMOVE, this.onMouseMove, this);
-
-        // Listen for mouseclicks and handle them accordingly
-        app.mouse.on(pc.EVENT_MOUSEDOWN, this.onMouseDown, this);
-    };
+        };
 
     Player.prototype = {
         name: "PlayerName",
         initialize: function() {},
         update: function(dt) {
-            this.handleCamera();
             this.handleMovement();
-        },
-        // FIXME: this and the onMouseMove need to be moved out to a camera
-        // handler rather than in player
-        handleCamera: function() {
-            var cameraPosition = this.camera.getPosition();
-            this.camera.setLocalEulerAngles(this.eulers.y, this.eulers.x, 0);
-            this.camera.setPosition(this.entity.getPosition());
-            this.camera.translateLocal(0, 2, 10);
-            this.camera.rotateLocal(-15, 0, 0);
-            if (this.camera.getPosition().y <= 0) {
-                this.camera.setPosition(cameraPosition.x, 0, cameraPosition.z);
-            }
-        },
-        onMouseMove: function(event) {
-            if (pc.Mouse.isPointerLocked() || event.buttons[0]) {
-                this.eulers.x -= this.lookSpeed * event.dx;
-                this.eulers.y -= this.lookSpeed * event.dy;
-
-                // clip camera on top
-                if (this.eulers.y <= -75) {
-                    this.eulers.y = -75;
-                } else if (this.eulers.y >= 45) {
-                    this.eulers.y = 45;
-                }
-                // reset left-right to 0 once a full circle is complete
-                if (this.eulers.x >= 360 || this.eulers.x <= -360) {
-                    this.eulers.x = 0;
-                }
-            }
-        },
-        onMouseDown: function(event) {
-            app.mouse.enablePointerLock();
         },
         handleMovement: function() {
             var forward = this.camera.forward;
