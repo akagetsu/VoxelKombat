d7a586223cc364a66dca7cd1aba0989645f60c79
Mike D <akagetsu@gmail.com>
2016-01-24 22:13:19 +0000

    Use socketio because socketio is awesome! :)

--- conn.js
+++ conn.js
@@ -1,10 +1,11 @@
+// conn.js
 pc.script.create("conn", function(app) {
 	var Conn = function(entity) {
 		this.entity = entity;
 	};
 	Conn.prototype = {
 		initialize: function() {
-			var socket = this.socket = new m.net.WebSocket({url: "ws://loclahost:3000"});
+			var socket = this.socket = io("http://localhost:3000");
 
 			// var socket = io();
 			socket.on('connect', function() {
