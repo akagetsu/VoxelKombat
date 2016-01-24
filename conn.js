5b13bde24cb3285c2cdb18afed72e36bfca1754e
Mike D <akagetsu@gmail.com>
2016-01-24 21:38:09 +0000

    Try some clientside comm out

--- conn.js
+++ conn.js
@@ -1,10 +1,27 @@
 pc.script.create("conn", function(app) {
 	var Conn = function(entity) {
+		this.entity = entity;
 	};
 	Conn.prototype = {
 		initialize: function() {
-			var socket = this.entity.script.socket.io.client.io();
-			console.log(socket);
+			var socket = this.socket = new m.net.WebSocket({url: "ws://loclahost:3000"});
+
+			// var socket = io();
+			socket.on('connect', function() {
+				console.log('connected');
+
+				socket.send('hello', {
+					name: 'Guest'
+				});
+			});
+
+			socket.on('welcome', function(data) {
+				console.log(data);
+			});
+		},
+		update: function(dt) {
+
 		}
 	};
+	return Conn;
 });
