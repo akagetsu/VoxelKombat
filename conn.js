c72495f8aa06d921b810d967d7eec75364e6834f
Mike D <akagetsu@gmail.com>
2016-02-05 20:20:54 +0000

    Send player id and color to backend

--- conn.js
+++ conn.js
@@ -1,26 +1,32 @@
 // conn.js
 
-pc.script.attribute("player","entity",null);
+pc.script.attribute("player", "entity", null);
 
 pc.script.create("conn", function(app) {
 	var Conn = function(entity) {
 		this.entity = entity;
+		this.player = null;
 	};
 	Conn.prototype = {
 		initialize: function() {
 			var socket = this.socket = io("http://localhost:3000");
-
-			// var socket = io();
+			var player = this.player;
 			socket.on('connect', function() {
 				console.log('connected');
 
-				socket.emit('hello', {
-					name: 'Guest'
+				socket.emit('user_join', {
+					id: socket.id,
+					color: player.model.material.name
 				});
-			});
 
-			socket.on('welcome', function(data) {
-				console.log(data);
+				socket.on('user_join', function(game) {
+					console.log(game);
+					var allUsers;
+					game.user_manager.users.forEach(function(user) {
+						allUsers += user.id + "\n";
+					});
+					document.getElementById('ui').innerHTML = "Users:\n" + allUsers;
+				});
 			});
 		},
 		update: function(dt) {
