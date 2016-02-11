43d39ed7270635f5eecec7a8395f192b08e529fe
Mike D <akagetsu@gmail.com>
2016-02-11 20:30:56 +0000

    Split some functionality and start sending player data to server

--- conn.js
+++ conn.js
@@ -2,44 +2,58 @@
 pc.script.create("conn", function(app) {
 	var Conn = function(entity) {
 		this.entity = entity;
+		this.playerControls = null;
 	};
 	Conn.prototype = {
 		initialize: function() {},
 		update: function(dt) {},
 		connectPlayer: function() {
-			var socket = this.socket = io("http://localhost:3000");
-			socket.on('connect', function() {
-				console.log('connected');
+			var socket = this.socket = io.connect("http://localhost:3000");
+			// socket.on('connect', function() {
+			// 	console.log('connected');
 
-				socket.emit('request_join');
+			socket.emit('request_join');
 
-				socket.on('accept_join', function(color) {
-					app.root.findByName('StartupCamera').enabled = false;
-					var camera = app.root.findByName('PlayerCamera').clone();
-					var player = app.root.findByName('Player').clone();
+			socket.on('accept_join', function(userData) {
+				app.root.findByName('StartupCamera').enabled = false;
+				var camera = app.root.findByName('PlayerCamera').clone();
+				var player = app.root.findByName('Player').clone();
 
-					camera.script.cameraControls.init(player);
-					app.root.findByName('Root').script.playerControls.init(player, camera);
-					player.model.materialAsset = app.assets.find(color[0].toUpperCase() + color.slice(1));
-					camera.enabled = true;
-					player.enabled = true;
+				camera.script.cameraControls.init(player);
+				this.playerControls = app.root.findByName('Root').script.playerControls;
+				this.playerControls.init(player, camera);
+				this.playerControls.playerData.uuid = userData.id;
+				this.playerControls.playerData.color = userData.color;
+				player.model.materialAsset = this.getMaterialOfColour(userData.color);
+				camera.enabled = true;
+				player.enabled = true;
 
-					app.root.addChild(player);
-					app.root.addChild(camera);
+				app.root.addChild(player);
+				app.root.addChild(camera);
 
-					document.getElementById('btn-connect').remove();
-				});
+				document.getElementById('btn-connect').remove();
+				this.playerControls = app.root.findByName('Root').script.playerControls;
+				this.sendPlayerData();
+			}.bind(this));
 
-				socket.on('user_update', function(data) {
-					console.log(data);
-				});
-
-				socket.on('refuse_join', function(res) {
-					if (res.status === "Error") {
-						console.error(res.msg);
-					}
-				})
+			socket.on('user_update', function(data) {
+				console.log(data);
 			});
+
+			socket.on('refuse_join', function(res) {
+				if (res.status === "Error") {
+					console.error(res.msg);
+				}
+			});
+			// });
+		},
+		sendPlayerData: function() {
+			var data = this.playerControls.playerData;
+			this.socket.emit('player_update', data);
+			setTimeout(this.sendPlayerData.bind(this), 100);
+		},
+		getMaterialOfColour: function(color) {
+			return app.assets.find(color[0].toUpperCase() + color.slice(1));
 		}
 	};
 	return Conn;
