f6e9341712bd3ee6ca7e5bbe5d5278a23e70ad67
Mike D <akagetsu@gmail.com>
2016-02-24 14:02:10 +0000

    Starting out some collision detection

---
+++ playerCollider.js
@@ -0,0 +1,20 @@
+pc.script.create("playerCollider", function (app) {
+    var PlayerCollider = function (entity) {
+        this.entity = entity;
+    };
+
+    PlayerCollider.prototype = {
+        initialize: function () {
+            this.entity.collision.on('collisionstart', this.onCollisionStart, this);
+        },
+
+        onCollisionStart: function (result) {
+            if (result.other.rigidbody && result.other.getName() === "Other Player") {
+                console.log("hit by", result.other.script.playerData.data.uuid);
+            }
+
+        }
+    };
+
+    return PlayerCollider;
+});
