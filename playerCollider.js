7a0f48f1816caf0b1ee92d324b8bc436aeb9278c
Mike D <akagetsu@gmail.com>
2016-03-20 19:54:46 +0000

    Send gameid and force of player upon collision as well

--- playerCollider.js
+++ playerCollider.js
@@ -11,7 +11,9 @@ pc.script.create("playerCollider", function(app) {
         onCollisionStart: function(result) {
             if (result.other.rigidbody && result.other.getName() === "Other Player") {
                 app.root.findByName("Root").script.conn.sendCollision({
+                    game: this.entity.script.playerData.data.gameId,
                     p1: this.entity.script.playerData.data.uuid,
+                    force: this.entity.rigidbody.linearVelocity,
                     p2: result.other.script.playerData.data.uuid
                 });
             }
