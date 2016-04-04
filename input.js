6e40e432c4a6376d7ce336b99c0dd91f645055e0
Mike D <akagetsu@gmail.com>
2016-04-04 16:27:18 +0100

    Start splitting input from player entity updates

---
+++ input.js
@@ -0,0 +1,52 @@
+// input.js
+pc.script.create("input", function(app) {
+
+	var Input = function(entity) {
+		this.entity = entity;
+		this.player = null;
+		this.controller = null;
+
+		app.keyboard.on(pc.EVENT_KEYDOWN, this.onKeyDown, this);
+		app.keyboard.on(pc.EVENT_KEYUP, this.onKeyUp, this);
+	};
+
+	Input.prototype = {
+		name: "Input",
+		initialize: function() {},
+		init: function(player) {
+			this.player = player;
+			this.controller = this.entity.script.controls;
+		},
+		update: function(dt) {},
+		onKeyDown: function(event) {
+			if(event.key === pc.KEY_W) {
+				this.controller.playerState.fow = true;
+			}
+			if(event.key === pc.KEY_S) {
+				this.controller.playerState.bck = true;
+			}
+			if(event.key === pc.KEY_A) {
+				this.controller.playerState.lef = true;
+			}
+			if(event.key === pc.KEY_D) {
+				this.controller.playerState.rig = true;
+			}
+		},
+		onKeyUp: function(event) {
+			if(event.key === pc.KEY_W) {
+				this.controller.playerState.fow = false;
+			}
+			if(event.key === pc.KEY_S) {
+				this.controller.playerState.bck = false;
+			}
+			if(event.key === pc.KEY_A) {
+				this.controller.playerState.lef = false;
+			}
+			if(event.key === pc.KEY_D) {
+				this.controller.playerState.rig = false;
+			}
+		}
+	};
+
+	return Input;
+});
